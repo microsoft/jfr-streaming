b6730459c629c9bca68b05f15c9ba1a3ff028f53
John Oliver <1615532+johnoliver@users.noreply.github.com>
2021-04-07 15:48:02 +0000

    Allow the JFR configuration to read from configuration files

--- src/main/java/com/microsoft/jfr/RecordingConfiguration.java
+++ src/main/java/com/microsoft/jfr/RecordingConfiguration.java
@@ -1,46 +1,104 @@
 package com.microsoft.jfr;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 /**
  * A flight recorder configuration controls the amount of data that is collected.
- * At this time, only pre-defined configurations are supported.
- * A pre-defined configuration is one which you could select with the 'settings' option
- * of the JVM option 'StartFlightRecording', for example {@code -XX:StartFlightRecording:settings=default.jfc}.
  */
-public class RecordingConfiguration {
+public abstract class RecordingConfiguration {
 
     /**
      * Convenience for selecting the pre-defined 'default' configuration that is standard with the JDK.
      * The default configuration is suitable for continuous recordings.
      */
-    public static final RecordingConfiguration DEFAULT_CONFIGURATION = new RecordingConfiguration("default");
+    public static final RecordingConfiguration DEFAULT_CONFIGURATION = new PredefinedConfiguration("default");
 
     /**
      * Convenience for referencing the 'profile' configuration that is standard with the JDK.
      * The profile configuration collects more events and is suitable for profiling an application.
      */
-    public static final RecordingConfiguration PROFILE_CONFIGURATION = new RecordingConfiguration("profile");
+    public static final RecordingConfiguration PROFILE_CONFIGURATION = new PredefinedConfiguration("profile");
 
     /**
-     * Sets a pre-defined configuration to use with a {@code Recording}.
-     * @param configurationName The name of the pre-defined configuration, not {@code null}.
+     * A pre-defined configuration is one which you could select with the 'settings' option
+     * of the JVM option 'StartFlightRecording', for example {@code -XX:StartFlightRecording:settings=default.jfc}.
+     */
+    static class PredefinedConfiguration extends RecordingConfiguration {
+        @Override
+        public String getMbeanSetterFunction() {
+            return "setPredefinedConfiguration";
+        }
+
+        /**
+         * Sets a pre-defined configuration to use with a {@code Recording}.
+         *
+         * @param configurationName The name of the pre-defined configuration, not {@code null}.
+         * @throws NullPointerException if predefinedConfiguration is {@code null}
+         */
+        public PredefinedConfiguration(String configurationName) {
+            super(configurationName);
+        }
+    }
+
+    /**
+     * A configuration that is read from a jfc file
+     */
+    static class JfcFileConfiguration extends RecordingConfiguration {
+        /**
+         * Sets a configuration from a jfc file to use with a {@code Recording}.
+         *
+         * @param configurationFile An InputStream containing the configuration file, not {@code null}.
+         * @throws NullPointerException if predefinedConfiguration is {@code null}
+         */
+        public JfcFileConfiguration(InputStream configurationFile) {
+            super(readConfigurationFile(configurationFile));
+        }
+
+        private static String readConfigurationFile(InputStream inputStream) {
+            if (inputStream != null) {
+                return new BufferedReader(new InputStreamReader(inputStream))
+                        .lines()
+                        .collect(Collectors.joining());
+            } else {
+                throw new IllegalArgumentException("Null configuration provided");
+            }
+        }
+
+        @Override
+        public String getMbeanSetterFunction() {
+            return "setConfiguration";
+        }
+    }
+
+    /**
+     * Get the setter function on the FlightRecorder mbean that receives this configuration type
+     * @return The setter function on the FlightRecorder mbean that receives this configuration type
+     */
+    public abstract String getMbeanSetterFunction();
+
+    /**
+     * Sets a configuration to use with a {@code Recording}.
+     * @param configuration The value of the configuration, not {@code null}.
      * @throws NullPointerException if predefinedConfiguration is {@code null}
      */
-    public RecordingConfiguration(String configurationName) {
-        Objects.requireNonNull(configurationName, "configurationName cannot be null");
-        this.name = configurationName;
+    public RecordingConfiguration(String configuration) {
+        Objects.requireNonNull(configuration, "configurationName cannot be null");
+        this.configuration = configuration;
     }
 
     /**
      * Get the name of the configuration. 
      * @return The name of the configuration.
      */
-    public String getName() {
-        return name;
+    public String getConfiguration() {
+        return configuration;
     }
 
-    // A configuration name, such as "default" or "profile"
-    private final String name;
+    // A configuration
+    protected final String configuration;
 
 }
